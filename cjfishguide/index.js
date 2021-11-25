25e559cc7e898ab6a573b05cf0737b5e5a6a90c8
Doris Morales <moralesdoris880@gmail.com>
2021-11-25 12:10:05 -0500

    Fishpedia Table Completed

--- cjfishguide/index.js
+++ cjfishguide/index.js
@@ -10,3 +10,52 @@ document.getElementById('navbar').addEventListener('click',(e)=>{
         document.getElementById('fish').style.display = "none";
     }
 })
+const table = document.getElementById('fish-table')
+
+fetchFish();
+function fetchFish(){
+fetch('https://acnhapi.com/v1a/fish',{
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(data => addFish(data))
+}
+
+function addFish(fish){
+    const table2 = document.getElementById('table-body')
+    for(let i=0; i<fish.length;i++){
+        let fishy = document.createElement('tr')
+        table2.appendChild(fishy)
+
+        let name = document.createElement('td')
+        name.innerHTML = fish[i].name['name-USen']
+        fishy.appendChild(name)
+
+        let location = document.createElement('td')
+        location.innerHTML = fish[i].availability.location
+        fishy.appendChild(location)
+
+        let shadowsize = document.createElement('td')
+        shadowsize.innerHTML = fish[i].shadow
+        fishy.appendChild(shadowsize)
+
+        let value = document.createElement('td')
+        value.innerHTML = fish[i].price
+        fishy.appendChild(value)
+
+        let time = document.createElement('td')
+        if (fish[i].availability.isAllDay){
+            time.innerHTML = "All Day"
+            fishy.appendChild(time)
+        }
+        else {
+            const timearray = fish[i].availability['time-array']
+            time.innerHTML = `${timearray[0]}- ${timearray[timearray.length-1]}`
+            fishy.appendChild(time)
+        }
+
+    }
+}
+
