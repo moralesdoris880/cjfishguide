a5a2f680cfd87a0e93b8173670b28a4b7d82e570
Doris Morales <moralesdoris880@gmail.com>
2021-11-28 22:03:36 -0500

    Drop selection working

--- cjfishguide/index.js
+++ cjfishguide/index.js
@@ -1,17 +1,32 @@
 document.getElementById('navbar').addEventListener('click',(e)=>{
     e.preventDefault();
     if(e.target.id === 'fish-page'){
-        document.getElementById('home').style.display = "none";
         document.getElementById('fish').style.display = "initial";
-        //document.getElementById('home').innerHTML.style.color = "#ff0000";
+        document.getElementById('home').style.display = "none";
+        document.getElementById('about').style.display = "none";
+        document.getElementById('events').style.display = "none";
     }
     else if(e.target.id === 'home-page'){
         document.getElementById('home').style.display = "initial";
         document.getElementById('fish').style.display = "none";
+        document.getElementById('about').style.display = "none";
+        document.getElementById('events').style.display = "none";
+    }
+    else if(e.target.id === 'events-page'){
+        document.getElementById('events').style.display = "initial";
+        document.getElementById('home').style.display = "none";
+        document.getElementById('fish').style.display = "none";
+        document.getElementById('about').style.display = "none";
+    }
+    else if(e.target.id === 'about-page'){
+        document.getElementById('about').style.display = "initial";
+        document.getElementById('home').style.display = "none";
+        document.getElementById('fish').style.display = "none";
+        document.getElementById('events').style.display = "none";
     }
 })
 const table = document.getElementById('fish-table')
-
+let fishData;
 fetchFish();
 function fetchFish(){
 fetch('https://acnhapi.com/v1a/fish',{
@@ -22,9 +37,9 @@ fetch('https://acnhapi.com/v1a/fish',{
 .then(response => response.json())
 .then(data => addFish(data))
 }
-
+const table2 = document.getElementById('table-body')
 function addFish(fish){
-    const table2 = document.getElementById('table-body')
+    fishData = fish;
     for(let i=0; i<fish.length;i++){
         let fishy = document.createElement('tr')
         table2.appendChild(fishy)
@@ -38,6 +53,7 @@ function addFish(fish){
 
         let name = document.createElement('td')
         name.innerHTML = fish[i].name['name-USen']
+        name.setAttribute('id',fish[i])
         fishy.appendChild(name)
 
         let location = document.createElement('td')
@@ -52,6 +68,18 @@ function addFish(fish){
         value.innerHTML = fish[i].price
         fishy.appendChild(value)
 
+        let north = document.createElement('p')
+        north.innerHTML = fish[i].availability['month-array-northern']
+        north.setAttribute('class','North')
+        north.style.display = 'none'
+        fishy.appendChild(north)
+
+        let south = document.createElement('p')
+        south.innerHTML = fish[i].availability['month-array-southern']
+        south.setAttribute('class','South')
+        south.style.display = 'none'
+        fishy.appendChild(south)
+
         let time = document.createElement('td')
         if (fish[i].availability.isAllDay){
             time.innerHTML = "All Day"
@@ -64,15 +92,45 @@ function addFish(fish){
         }
     }
 }
-addMonth();
-function addMonth(){
-    const monthlist =[ "January", "February", "March", "April", "May", "June", 
-    "July", "August", "September", "October", "November", "December" ];
-    for(let i =0;i<12;i++)
-    {   
-        let option = document.createElement('option')
-        option.innerHTML = monthlist[i]
-        document.getElementById('months').appendChild(option)
-    }
-}
+
+const form = document.getElementById('month-selection')
+const months = document.querySelector('#months')
+const hemispheres = document.querySelector('#hemispheres')
+
+let selectedMonth ='1'
+let selectedHemisphere = 'month-array-northern'
+
+months.addEventListener('change',(e)=> {
+    selectedMonth = e.target.value
+})
+hemispheres.addEventListener('change',(e)=> {
+    selectedHemisphere = e.target.value
+})
+
+form.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+
+    let filteredFishies = fishData.filter(fish => {
+      let fishHemisphere = fish.availability[selectedHemisphere]
+
+      let hasFishOnThatDay = fishHemisphere.some(availableDay=>{
+
+       let hasMatchingDay = availableDay == selectedMonth   
+        return hasMatchingDay
+      })
+       return hasFishOnThatDay
+    })
+
+    table2.innerHTML = null;
+    addFish(filteredFishies);
+
+    console.log(filteredFishies, selectedMonth, selectedHemisphere)   
+
+})
+
+//CHECK ROW
+//CHECK P TAG OF NORTH TO SEE IF IT MATCHES WITH MONTH
+//IF IT DOES KEEP
+//ELSE DISPLAY NONE
 
